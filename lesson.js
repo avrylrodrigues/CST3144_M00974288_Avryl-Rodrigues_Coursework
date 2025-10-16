01e34891de025d42a825c9e079dd59741ee3305f
Avryl Rodrigues <avryl.r123@gmail.com>
2025-10-16 09:45:19 +0400

    updated files

---
+++ lesson.js
@@ -0,0 +1,113 @@
+let lesson = [
+    {
+        "id":1001,
+        "Subject":"Music",
+        "Location":"Dubai",
+        "Price":100,
+        "Icon":"images/music.png",
+        "availableSeats":5
+    },
+    {
+        "id": 1002,
+        "Subject": "Football",
+        "Location": "Dubai",
+        "Price": 250,
+        "Icon": "images/football.png",
+        "availableSeats": 5
+    },
+    {
+        "id": 1003,
+        "Subject": "Crafts",
+        "Location": "Dubai",
+        "Price": 250,
+        "Icon": "images/crafts.png",
+        "availableSeats": 5
+    },
+    {
+        "id": 1004,
+        "Subject": "Gymnastics",
+        "Location": "Dubai",
+        "Price": 250,
+        "Icon": "images/gymnastics.png",
+        "availableSeats": 5
+    },
+    {
+        "id": 1005,
+        "Subject": "Cooking",
+        "Location": "Dubai",
+        "Price": 250,
+        "Icon": "images/cooking.png",
+        "availableSeats": 5
+    },
+    {
+        "id": 1006,
+        "Subject": "Photography",
+        "Location": "Dubai",
+        "Price": 250,
+        "Icon": "images/photography.png",
+        "availableSeats": 5
+    },
+    {
+        "id": 1007,
+        "Subject": "Writing",
+        "Location": "Dubai",
+        "Price": 250,
+        "Icon": "images/writing.png",
+        "availableSeats": 5
+    },
+    {
+        "id": 1008,
+        "Subject": "Theatre",
+        "Location": "Dubai",
+        "Price": 250,
+        "Icon": "images/theatre.png",
+        "availableSeats": 5
+    },
+    {
+        "id": 1009,
+        "Subject": "Badminton",
+        "Location": "Dubai",
+        "Price": 250,
+        "Icon": "images/badminton.png",
+        "availableSeats": 5
+    },
+    {
+        "id": 1010,
+        "Subject": "Dance",
+        "Location": "Dubai",
+        "Price": 450,
+        "Icon": "images/dance.png",
+        "availableSeats": 5
+    },
+    {
+        "id": 1011,
+        "Subject": "Gaming",
+        "Location": "Dubai",
+        "Price": 450,
+        "Icon": "images/gaming.png",
+        "availableSeats": 5
+    },{
+        "id": 1012,
+        "Subject": "Art",
+        "Location": "Dubai",
+        "Price": 250,
+        "Icon": "images/art.png",
+        "availableSeats": 5
+    },
+    {
+        "id": 1013,
+        "Subject": "Yoga",
+        "Location": "Dubai",
+        "Price": 450,
+        "Icon": "images/yoga.png",
+        "availableSeats": 5
+    },
+    {
+        "id": 1014,
+        "Subject": "Film Making",
+        "Location": "Dubai",
+        "Price": 450,
+        "Icon": "images/film-making.png",
+        "availableSeats": 5
+    }
+]
