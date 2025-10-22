ac2d1f4fcf6dfb49165d5f9d3047edcd87ad95fd
Avryl Rodrigues <avryl.r123@gmail.com>
2025-10-22 17:24:54 +0400

    updated files

--- lesson.js
+++ lesson.js
@@ -10,7 +10,7 @@ let lesson = [
     {
         "id": 1002,
         "Subject": "Football",
-        "Location": "Dubai",
+        "Location": "Abu Dhabi",
         "Price": 85,
         "Icon": "images/football.png",
         "availableSeats": 5
@@ -26,7 +26,7 @@ let lesson = [
     {
         "id": 1004,
         "Subject": "Gymnastics",
-        "Location": "Dubai",
+        "Location": "Sharjah",
         "Price": 40,
         "Icon": "images/gymnastics.png",
         "availableSeats": 5
@@ -34,7 +34,7 @@ let lesson = [
     {
         "id": 1005,
         "Subject": "Cooking",
-        "Location": "Dubai",
+        "Location": "Abu Dhabi",
         "Price": 75,
         "Icon": "images/cooking.png",
         "availableSeats": 5
@@ -50,7 +50,7 @@ let lesson = [
     {
         "id": 1007,
         "Subject": "Writing",
-        "Location": "Dubai",
+        "Location": "Ajman",
         "Price": 20,
         "Icon": "images/writing.png",
         "availableSeats": 5
@@ -58,7 +58,7 @@ let lesson = [
     {
         "id": 1008,
         "Subject": "Theatre",
-        "Location": "Dubai",
+        "Location": "Ras Al Khaimah",
         "Price": 60,
         "Icon": "images/theatre.png",
         "availableSeats": 5
@@ -66,7 +66,7 @@ let lesson = [
     {
         "id": 1009,
         "Subject": "Badminton",
-        "Location": "Dubai",
+        "Location": "Abu Dhabi",
         "Price": 60,
         "Icon": "images/badminton.png",
         "availableSeats": 5
@@ -74,7 +74,7 @@ let lesson = [
     {
         "id": 1010,
         "Subject": "Dance",
-        "Location": "Dubai",
+        "Location": "Sharjah",
         "Price": 50,
         "Icon": "images/dance.png",
         "availableSeats": 5
@@ -82,14 +82,14 @@ let lesson = [
     {
         "id": 1011,
         "Subject": "Gaming",
-        "Location": "Dubai",
+        "Location": "Ajman",
         "Price": 90,
         "Icon": "images/gaming.png",
         "availableSeats": 5
     },{
         "id": 1012,
         "Subject": "Art",
-        "Location": "Dubai",
+        "Location": "Fujairah",
         "Price": 45,
         "Icon": "images/art.png",
         "availableSeats": 5
@@ -97,7 +97,7 @@ let lesson = [
     {
         "id": 1013,
         "Subject": "Yoga",
-        "Location": "Dubai",
+        "Location": "Umm Al Quwain",
         "Price": 25,
         "Icon": "images/yoga.png",
         "availableSeats": 5
